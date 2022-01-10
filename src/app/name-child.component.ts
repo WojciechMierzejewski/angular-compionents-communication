07bb6837eee896b01b0e2560f3160ea4dcf685e9
WojciechMierzejewski <s21617@pjwstk.edu.pl>
2022-01-10 13:02:07 +0100

    no name trimming

--- src/app/name-child.component.ts
+++ src/app/name-child.component.ts
@@ -9,7 +9,7 @@ export class NameChildComponent {
   @Input()
   get name(): string { return this._name; }
   set name(name: string) {
-    this._name = (name && name.trim()) || '<no name set>';
+    this._name = (name || '<no name set>';
   }
   private _name = '';
 }
